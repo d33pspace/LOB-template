84f89d4cb5fe41028fa42aeb45849c2e2e2128b1
Edward Zhao <edward.zhao@dbschenker.com>
2024-01-11 21:13:04 +0800

    keep fix

--- 2024_01_reports/email/ZapierPy.py
+++ 2024_01_reports/email/ZapierPy.py
@@ -53,12 +53,15 @@ def translate_currency(currency_set, currency=None):
         # If currency is not provided, pop the single currency from the set
         currency = next(iter(currency_set), None)
 
-    if currency == 'USD':
+    if len(currency_set) > 1:
         return 'USD' if preferred_language == 'en' else '美元'
-    elif currency == 'CNY':
-        return 'CNY' if preferred_language == 'en' else '人民币'
     else:
-        return currency
+        if currency == 'USD':
+            return 'USD' if preferred_language == 'en' else '美元'
+        elif currency == 'CNY':
+            return 'CNY' if preferred_language == 'en' else '人民币'
+        else:
+            return currency
 
 
 def read_resource(url):
@@ -73,94 +76,120 @@ def read_resource(url):
         content = response.text
 
     # Check if the resource is a JSON file based on the file extension
-    if url.endswith(".json") or (local_mode and filename.endswith(".json")):
-        return json.loads(content)
+    # if url.endswith(".json") or (local_mode and filename.endswith(".json")):
+    #     return json.loads(content)
+    # else:
+    return content
+
+
+def compose_html():
+    preferred_language = 'zh' if "preferred_language" in input_data and 'zh' in input_data[
+        "preferred_language"] else 'en'
+    from_email = 'connect@renewal.org.cn'
+    mailTo = input_data["mail_to"]
+    contactName = jsonObject["contactName"]
+    salutation = input_data["salutation"]
+    salutation = contactName if salutation is None or salutation == "" else salutation
+
+    public_page_url = "https://d33pspace.github.io/LOB-template"
+    main_template_url = f'{public_page_url}/2024_01_reports/email/email_report_{preferred_language}.html'
+    email_report_en_line_item_multi_currency_url = f'{public_page_url}/2024_01_reports/email/email_report_{preferred_language}_line_item_multi_currency_template.html'
+    email_report_en_line_item_single_currency_url = f'{public_page_url}/2024_01_reports/email/email_report_{preferred_language}_line_item_single_currency_template.html'
+
+    main_html_template = read_resource(main_template_url)
+    email_report_en_line_item_multi_currency_template = read_resource(email_report_en_line_item_multi_currency_url)
+    email_report_en_line_item_single_currency_url = read_resource(email_report_en_line_item_single_currency_url)
+
+    unique_currencies = set()
+    invoiceTotalUSD_Count = 0
+    unitPriceSource_Count = 0
+    for line_item in jsonObject['lineItems']:
+        unique_currencies.add(line_item['originalCurrency'])
+        invoiceTotalUSD_Count += float(line_item['invoiceTotalUSD'])
+        unitPriceSource_Count += float(line_item['unitPriceSource'])
+
+    line_item_template_to_use = email_report_en_line_item_single_currency_url if len(unique_currencies) == 1 \
+        else email_report_en_line_item_multi_currency_template
+
+    amount_format = "{:,.2f}"
+    total_giving = "{:,.2f} {}"
+    if len(unique_currencies) == 1:
+        total_giving = total_giving.format(unitPriceSource_Count, translate_currency(unique_currencies))
     else:
-        return content
+        total_giving = total_giving.format(invoiceTotalUSD_Count, translate_currency(unique_currencies))
+
+    # List to store generated HTML content
+    line_items_content_array = []
+    # Iterate through each line item in the JSON object
+    for line_item in jsonObject['lineItems']:
+        # Replace placeholders in the HTML template with values from the JSON object
+        html_content = line_item_template_to_use.replace('{{ 202401_report_date }}',
+                                                         format_date_of_gift(line_item['invoiceDate']))
+        html_content = html_content.replace('{{ 202401_report_transaction_reference }}',
+                                            line_item['invoiceNumber'].replace("INV", "RWL"))
+        html_content = html_content.replace('{{ 202401_report_amount }}',
+                                            amount_format.format(float(line_item['unitPriceSource'])))
+        html_content = html_content.replace('{{ 202401_report_currency }}',
+                                            translate_currency(unique_currencies, line_item['originalCurrency']))
+        html_content = html_content.replace('{{ 202401_report_exchange_rate }}',
+                                            '1' if line_item['originalCurrency'] == 'USD' else str(
+                                                line_item['currencyRate']))
+        html_content = html_content.replace('{{ 202401_report_usd_amount }}',
+                                            amount_format.format(float(line_item['invoiceTotalUSD'])))
+        html_content = html_content.replace('{{ 202401_report_description }}',
+                                            translate_description(line_item['description']))
+        html_content = html_content.replace('{{ 202401_report_method }}',
+                                            translate_payment_method(line_item['method'], line_item['reference']))
+
+        # Append the generated HTML content to the list
+        line_items_content_array.append(html_content)
+
+    # Concatenate all HTML contents into one string
+    line_items_html_content = "\n".join(line_items_content_array)
+
+    main_html_content = main_html_template.replace('{{ 202401_report_salutation }}', salutation)
+    main_html_content = main_html_content.replace('{{ 202401_report_donor_name }}', contactName)
+    main_html_content = main_html_content.replace('{{ 202401_report_line_items }}', line_items_html_content)
+    main_html_content = main_html_content.replace('{{ 202401_report_total_giving }}', total_giving)
+    main_html_content = main_html_content.replace('{{ from_email }}', from_email)
+    main_html_content = main_html_content.replace('{{ subscriber.email }}', mailTo)
+    main_html_content = main_html_content.replace('{{ subscriber.salutation }}', salutation)
+    main_html_content = main_html_content.replace('{{ inline_postal_address }}',
+                                                  '548 Market St # 54802, San Francisco CA, 94104, USA')
+
+    email_subject = '您的2023捐赠报告' if preferred_language == 'zh' else 'Your 2023 giving report'
+
+    if local_mode:
+        output_file_name = f'test_{preferred_language}.html'
+        with open(output_file_name, 'w', encoding='utf-8') as output_file:
+            output_file.write(main_html_content)
+        print(f'The final HTML content has been written to {output_file_name}')
+
+    return {"email_content": main_html_content, "email_subject": email_subject}
 
 
 if local_mode:
-    read_json_object = json.dumps(read_resource("input.json"))
+    read_json_object = read_resource("input.json")
+    # read_json_object = json.dumps(read_resource("input.json"))
     input_data = {
         "json_object": read_json_object,
-        "preferred_language": "zh",
+        "preferred_language": "en-us",
         "mail_to": "edwazhao@hotmail.com",
         "salutation": "xxx"
     }
 
-jsonObject = json.loads(input_data["json_object"])
-preferred_language = input_data["preferred_language"]
-preferred_language = 'zh' if 'zh' in preferred_language else 'en'
-from_email = 'connect@renewal.org.cn'
-mailTo = input_data["mail_to"]
-contactName = jsonObject["contactName"]
-salutation = input_data["salutation"]
-salutation = contactName if salutation is None or salutation == "" else salutation
+preferred_language = "en"
+jsonObject = {}
+json_error = ''
+try:
+    # Try to parse the JSON string
+    jsonObject = json.loads(input_data["json_object"])
+except ValueError as e:
+    # Handle the exception when the JSON string is not valid
+    json_error = f"Error decoding JSON: {e}"
 
-public_page_url="https://d33pspace.github.io/LOB-template"
-main_template_url = f'{public_page_url}/2024_01_reports/email/email_report_{preferred_language}.html'
-email_report_en_line_item_multi_currency_url = f'{public_page_url}/2024_01_reports/email/email_report_{preferred_language}_line_item_multi_currency_template.html'
-email_report_en_line_item_single_currency_url = f'{public_page_url}/2024_01_reports/email/email_report_{preferred_language}_line_item_single_currency_template.html'
-
-main_html_template = read_resource(main_template_url)
-email_report_en_line_item_multi_currency_template = read_resource(email_report_en_line_item_multi_currency_url)
-email_report_en_line_item_single_currency_url = read_resource(email_report_en_line_item_single_currency_url)
-
-unique_currencies = set()
-invoiceTotalUSD_Count = 0
-unitPriceSource_Count = 0
-for line_item in jsonObject['lineItems']:
-    unique_currencies.add(line_item['originalCurrency'])
-    invoiceTotalUSD_Count += float(line_item['invoiceTotalUSD'])
-    unitPriceSource_Count += float(line_item['unitPriceSource'])
-
-line_item_template_to_use = email_report_en_line_item_single_currency_url if len(unique_currencies) == 1 \
-    else email_report_en_line_item_multi_currency_template
-
-amount_format = "{:,.2f}"
-total_giving = "{:,.2f} {}"
-if len(unique_currencies) == 1:
-    total_giving = total_giving.format(unitPriceSource_Count, translate_currency(unique_currencies))
+output = {}
+if "contactName" in jsonObject:
+    output = compose_html()
 else:
-    total_giving = total_giving.format(invoiceTotalUSD_Count, translate_currency(unique_currencies))
-
-
-# List to store generated HTML content
-line_items_content_array = []
-# Iterate through each line item in the JSON object
-for line_item in jsonObject['lineItems']:
-    # Replace placeholders in the HTML template with values from the JSON object
-    html_content = line_item_template_to_use.replace('{{ 202401_report_date }}', format_date_of_gift(line_item['invoiceDate']))
-    html_content = html_content.replace('{{ 202401_report_transaction_reference }}', line_item['invoiceNumber'].replace("INV", "RWL"))
-    html_content = html_content.replace('{{ 202401_report_amount }}', amount_format.format(float(line_item['unitPriceSource'])))
-    html_content = html_content.replace('{{ 202401_report_currency }}', translate_currency(unique_currencies, line_item['originalCurrency']))
-    html_content = html_content.replace('{{ 202401_report_exchange_rate }}', '1' if line_item['originalCurrency'] == 'USD' else str(line_item['currencyRate']))
-    html_content = html_content.replace('{{ 202401_report_usd_amount }}', amount_format.format(float(line_item['invoiceTotalUSD'])))
-    html_content = html_content.replace('{{ 202401_report_description }}', translate_description(line_item['description']))
-    html_content = html_content.replace('{{ 202401_report_method }}', translate_payment_method(line_item['method'], line_item['reference']))
-
-    # Append the generated HTML content to the list
-    line_items_content_array.append(html_content)
-
-# Concatenate all HTML contents into one string
-line_items_html_content = "\n".join(line_items_content_array)
-
-main_html_content = main_html_template.replace('{{ 202401_report_salutation }}', salutation)
-main_html_content = main_html_content.replace('{{ 202401_report_donor_name }}', contactName)
-main_html_content = main_html_content.replace('{{ 202401_report_line_items }}', line_items_html_content)
-main_html_content = main_html_content.replace('{{ 202401_report_total_giving }}', total_giving)
-main_html_content = main_html_content.replace('{{ from_email }}', from_email)
-main_html_content = main_html_content.replace('{{ subscriber.email }}', mailTo)
-main_html_content = main_html_content.replace('{{ subscriber.salutation }}', salutation)
-main_html_content = main_html_content.replace('{{ inline_postal_address }}', '548 Market St # 54802, San Francisco CA, 94104, USA')
-
-email_subject = '您的2023捐赠报告' if preferred_language == 'zh' else 'Your 2023 giving report'
-
-output = {"email_content": main_html_content, "email_subject": email_subject}
-
-if local_mode:
-    output_file_name = f'test_{preferred_language}.html'
-    with open(output_file_name, 'w', encoding='utf-8') as output_file:
-        output_file.write(main_html_content)
-    print(f'The final HTML content has been written to {output_file_name}')
-
+    output = {"error": json_error, "email_content": "", "email_subject": ""}
