7478c37e634590a932a21b26a176948eabbb3a6d
Edward Zhao <edward.zhao@dbschenker.com>
2024-05-06 21:57:05 +0800

    update text

--- zap_send_wechat_msg/ZapierSendWechat.py
+++ zap_send_wechat_msg/ZapierSendWechat.py
@@ -8,14 +8,7 @@ local_mode = False  # True or False
 if "local_mode=true" in sys.argv:
     local_mode = True
 
-
-def format_date_of_gift():
-    today_date = datetime.now()
-    formatted_date = today_date.strftime("%Y-%m-%d")
-    return formatted_date
-
-
-def send_wechat_messsage(input_obj):
+def send_wechat_message(input_obj):
     response = {"status_code": -999}
 
     url = 'https://renewal.deepspace.org.cn/api/v1/work-tool/send-message'
@@ -28,13 +21,24 @@ def send_wechat_messsage(input_obj):
     }
     try:
         # Construct the text with values from input_obj
-        text = "Contributor: {}\nDate of gift: {}\nAmount: {}\nMethod: {}\nReference: {}".format(
-            input_obj.get("contributor", ""),
-            format_date_of_gift(),
-            input_obj.get("amount", ""),
-            input_obj.get("method", ""),
-            input_obj.get("reference", "")
-        )
+        if 'cn' in input_obj.get("preferred_language", ""):
+            text = "{}\n\n捐款者: {}\n捐款日期: {}\n捐款金额: {}\n捐款方式: {}\n捐款编号: {}".format(
+                input_obj.get("text", ""),
+                input_obj.get("contributor", ""),
+                input_obj.get("date", ""),
+                input_obj.get("amount", ""),
+                input_obj.get("method", ""),
+                input_obj.get("reference", "")
+            )
+        else:
+            text = "{}\n\nContributor: {}\nDate of gift: {}\nAmount: {}\nMethod: {}\nReference: {}".format(
+                input_obj.get("text", ""),
+                input_obj.get("contributor", ""),
+                input_obj.get("date", ""),
+                input_obj.get("amount", ""),
+                input_obj.get("method", ""),
+                input_obj.get("reference", "")
+            )
 
         data = json.dumps({
             "phoneNumber": input_obj.get("phone_number", ""),
@@ -50,7 +54,7 @@ def send_wechat_messsage(input_obj):
     except Exception as e:
         print("Error occurred: {}".format(str(e)))
 
-    return {"command_id": response.json().get("command_id", ""), "message": response.json().get("message", ""),
+    return {"command_id": str(response.json().get("command_id", "")), "message": response.json().get("message", ""),
             "code": response.status_code}
 
 
@@ -61,12 +65,14 @@ if local_mode:
     input_data = {
         "phone_number": "15250982865",
         "wechat_nickname": "名称2865",
-        "preferred_language": "en-us",
-        "contributor": "James Edward McWhinney (0484)",
-        "amount": "11.12",
+        "preferred_language": "zh-cn",
+        "contributor": "James (0484)",
+        "amount": "10.12",
         "method": "Alipay",
-        "reference": "REL-808111"
+        "reference": "REL-808122",
+        "text": "Eddie, thank you for your support.",
+        "date": "2024-05-01"
     }
 
-output = send_wechat_messsage(input_data)
+output = send_wechat_message(input_data)
 print(output)
