90776321c11e6f1cc3cd9ed362421d7bb1f084fe
Edward Zhao <edward.zhao@dbschenker.com>
2024-05-07 21:59:59 +0800

    update text

--- zap_send_wechat_msg/ZapierSendWechat.py
+++ zap_send_wechat_msg/ZapierSendWechat.py
@@ -22,8 +22,9 @@ def send_wechat_message(input_obj):
     try:
         # Construct the text with values from input_obj
         if 'cn' in input_obj.get("preferred_language", ""):
-            text = "{}\n\n捐款者: {}\n捐款日期: {}\n捐款金额: {}\n捐款方式: {}\n捐款编号: {}".format(
-                input_obj.get("text", ""),
+            text = "{}， {}\n\n捐款者： {}\n捐款日期： {}\n捐款金额： {}\n捐款方式： {}\n捐款编号： {}".format(
+                input_obj.get("salutation", ""),
+                input_obj.get("message", ""),
                 input_obj.get("contributor", ""),
                 input_obj.get("date", ""),
                 input_obj.get("amount", ""),
@@ -31,8 +32,9 @@ def send_wechat_message(input_obj):
                 input_obj.get("reference", "")
             )
         else:
-            text = "{}\n\nContributor: {}\nDate of gift: {}\nAmount: {}\nMethod: {}\nReference: {}".format(
-                input_obj.get("text", ""),
+            text = "{}, {}\n\nContributor: {}\nDate of gift: {}\nAmount: {}\nMethod: {}\nReference: {}".format(
+                input_obj.get("salutation", ""),
+                input_obj.get("message", ""),
                 input_obj.get("contributor", ""),
                 input_obj.get("date", ""),
                 input_obj.get("amount", ""),
@@ -64,13 +66,14 @@ def send_wechat_message(input_obj):
 if local_mode:
     input_data = {
         "phone_number": "15250982865",
-        "wechat_nickname": "名称2865",
+        "wechat_nickname": "群聊测试",
         "preferred_language": "zh-cn",
-        "contributor": "James (0484)",
-        "amount": "10.12",
+        "contributor": "Edward Zhao",
+        "amount": "10.12CNY",
         "method": "Alipay",
         "reference": "REL-808122",
-        "text": "Eddie, thank you for your support.",
+        "salutation": "Edward",
+        "message": "thank you so much for your gift. You're really doing a lot for people in need at the Renewal Center.",
         "date": "2024-05-01"
     }
 
