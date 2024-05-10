88a0f0f647ffc8e74926ba1ea1ce46c9a2fc0e4a
Edward Zhao <edward.zhao@dbschenker.com>
2024-05-10 23:14:02 +0800

    UpdateWebsiteFromHobspot.py

--- hubspot/UpdateWebsiteFromHobspot.py
+++ hubspot/UpdateWebsiteFromHobspot.py
@@ -10,18 +10,26 @@ if "local_mode=true" in sys.argv:
 
 
 def update_website(input_obj):
-    url = 'https://renewal.deepspace.org.cn/api/v1/user/update-from-hubspot'
-    if local_mode:
-        url = 'http://localhost:8118/v1/user/update-from-hubspot'
+
+    if "email" not in input_obj or not input_obj["email"]:
+        return {"update_count": 0, "message": "update failed because empty email"}
+
+    if input_obj.get("email", "").endswith("@alt.renewal.org.cn"):
+        url = 'https://renewal.deepspace.org.cn/api/v1/user/update-from-hubspot'
+    else:
+        url = 'https://test2.renewal.org.cn/api/v1/user/update-from-hubspot'
+
+    # if local_mode:
+    #     url = 'http://localhost:8118/v1/user/update-from-hubspot'
 
     headers = {
         'Content-Type': 'application/json',
         'Authorization': 'API gzWGFkOzdPqrr8DiNYbWJjNGExMDczNmVlNzU3NzoXOTeJDYyz'
     }
     try:
-        if input_data["receipt_preference"] == "N":
+        if input_obj["receipt_preference"] == "N":
             approach_preference = "None"
-        elif input_data["wechat_preference"] == "Y" or input_data["email_preference"] == "Y":
+        elif input_obj["wechat_preference"] == "Y" or input_obj["email_preference"] == "Y":
             approach_preference = "ALL"
         else:
             approach_preference = "RECEIPT_ONLY"
@@ -36,14 +44,11 @@ def update_website(input_obj):
         })
 
         # Make a POST request to the API endpoint with the defined headers and data
-        if "email" not in input_data or not input_data["email"]:
-            result_json = {"update_count": 0, "message": "update failed because empty email"}
-        else:
-            response = requests.post(url, headers=headers, data=data)
-            result_json = {"update_count": response.json().get("update_count", 0), "message": "website processed"}
+        response = requests.post(url, headers=headers, data=data)
+        result_json = {"update_count": response.json().get("update_count", 0), "message": "website processed"}
     except Exception as e:
         print("Error occurred: {}".format(str(e)))
-        result_json = {"update_count": 0, "message": "update failed because " + str(e)}
+        result_json = {"update_count": 0, "message": "update failed with exception, because " + str(e)}
 
     return result_json
 
@@ -58,8 +63,8 @@ if local_mode:
         "wechat_preference": "Y",
         "email_preference": "N",
         "preferred_language": "zh-cn",
-        "receipt_name": "Test Z",
-        "salutation": "et"
+        "receipt_name": "Test Z1",
+        "salutation": "et1"
     }
 
 output = update_website(input_data)
